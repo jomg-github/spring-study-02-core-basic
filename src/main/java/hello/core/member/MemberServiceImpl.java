526af529e47f44a702d94ef5a8948a63bde0a26b
조민기 <jomg@jomgui-MacBookPro.local>
2023-04-11 00:01:54 +0900

    #01-1 예제 만들기 - 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import hello.core.member.MemberService;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long id) {
+        return memberRepository.findById(id);
+    }
+}
