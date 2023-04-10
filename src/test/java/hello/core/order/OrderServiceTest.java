397a06c60b190c40e3a4f1b6279d79f73a60d9ec
조민기 <hodduco_o@naver.com>
2023-04-11 00:41:23 +0900

    01-2 예제 만들기 - 주문, 할인 도메인 개발

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        memberService.join(new Member(memberId, "member1", Grade.VIP));
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
